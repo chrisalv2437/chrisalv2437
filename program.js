3f3f644835c7fabfdbd529f6f87330c50a155ee5
chrisalv2437 <162181147+chrisalv2437@users.noreply.github.com>
2024-03-06 12:33:06 -0800

    Two new files that simply displays the JSON data on the web page.

---
+++ program.js
@@ -0,0 +1,73 @@
+/////////////////////////////////////
+//Name: Christian Alvarado
+//Filename: api.js
+//Editor: Visual Studio Code
+//Language: Javascript
+//Extension: Live server
+//OS: Windows 10
+//Purpose: reaching out to randomapi.com, fetching the data in JSON format,
+//print out a user friendly interface that will sort data for user
+//Operational status: not working
+/////////////////////////////////////
+
+//Defining API URL to call
+const url = "https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb?fmt=json";
+//const dataDisplay = document.getElementById('dataDisplay');
+
+//Fetch function to make a GET request to API URL returning a promise
+fetch(url)
+    //then method handles the asynchoronous response from server
+    .then(response => {
+        //checks to ensure response is valid
+        if(!response.ok){
+            if(response.status === 404){
+                throw new Error('Data not found');
+            } else if(response.status === 500){
+                throw new Error('Server error');
+            } else{
+                throw new Error ('Response was not ok');
+        }
+    }
+        //parse the JSON data using method below
+        return response.json()
+    })
+    //displaying the data onto the webpage
+    .then(data => {
+        console.log(data);
+        const dataDisplay = document.getElementById('dataDisplay');
+        const informationElement = document.createElement("p");
+        informationElement.textContent = "Info: " + JSON.stringify(data.results);
+        dataDisplay.appendChild(informationElement);
+        console.log(data.results);
+        
+    })
+    //handle any errors that may occur
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+function compareFirstName(a, b){
+    a = a.toLowerCase();
+    b = b.toLowerCase();
+
+    if(a.first < b.first){
+        return -1;
+    }
+    if(a.first > b.first){
+        return 1;
+    }
+    return 0;
+}
+
+function compareLastName(a, b){
+    a = a.toLowerCase();
+    b = b.toLowerCase();
+
+    if(a.last < b.last){
+        return -1;
+    }
+    if(a.last > b.last){
+        return 1;
+    }
+    return 0;
+}
