23a7b4be8dd29f79051baf124104462c990307c9
Marshall Smith <marshallksmith16@gmail.com>
2024-03-08 15:30:42 -0600

    Updated code Went through and updated the some of the code. Super close. Good Job!

--- program.js
+++ program.js
@@ -1,4 +1,5 @@
 //Fetch function to make a GET request to API URL returning a promise
+let myJson = {};
 async function getResults(){
 
     const dataDisplay = document.getElementById('dataDisplay');
@@ -31,63 +32,79 @@ async function getResults(){
 
         //defining results to sort
         let myResults = data.results;
-        let element = myResults[0];
+        myJson = myResults[0];
 
         //displaying information
         const dataDisplay = document.getElementById('dataDisplay');
         const informationElement = document.createElement("p");
-        informationElement.textContent = "" + JSON.stringify(element);
-        dataDisplay.appendChild(informationElement);
-
-        //function compares the first names and sorts it from a to z
-        function sortingFirstNames(a, b){
-            if(a.first < b.first){
-                return -1;
-            }
-            if(a.first > b.first){
-                return 1;
-            }
-            return 0;
-        }
-
-        //function compares the last names and sorts it from a to z
-        function sortingLastNames(a, b){
-            if(a.last < b.last){
-                return -1;
-            }
-            if(a.last > b.last){
-                return 1;
-            }
-            return 0;
-        }
-
-        //function does not work correctly as the json data is a string
-        function sortBalance(a, b){
-            if(a.balance < b.balance){
-                return -1;
-            }
-            if(a.balance > b.balance){
-                return 1;
-            }
-            return 0;
-        }
-
-        //function compares the dates and sorts them from awhile ago to recently
-        function compareDates(a, b){
-            return new Date(a.created).getTime() - new Date(b.created).getTime();
-        }
-
-        //using sort function for first names
-        element.sort(sortingFirstNames);
-        
-        //logging it to the console and presenting it in table format
-        console.table(element);
+        informationElement.textContent = "" + JSON.stringify(myJson);
+        dataDisplay.innerHTML = JSON.stringify(myJson);
 
     })
     //handle any errors that may occur
     .catch(error => {
         console.error('Error:', error);
     });
+
+    const myList = document.getElementById('myList');
+    myList.onchange = function(){
+        console.log('Sort by: ', myList.value)
+        let value = myList.value;
+        if(value === 'First Name'){
+            myJson.sort(sortingFirstNames);
+        }
+        if(value === 'Last Name'){
+            myJson.sort(sortingLastNames);
+        }
+        if(value === 'Balance'){
+            myJson.sort(sortBalance);
+        }
+        if(value === 'Creation Date'){
+            myJson.sort(compareDates);
+        }
+
+        let dataDisplay = document.getElementById('dataDisplay');
+        dataDisplay.innerHTML = JSON.stringify(myJson);
+        console.table(myJson);
+    };
+
+     //function compares the first names and sorts it from a to z
+     function sortingFirstNames(a, b){
+        if(a.first < b.first){
+            return -1;
+        }
+        if(a.first > b.first){
+            return 1;
+        }
+        return 0;
+    }
+
+    //function compares the last names and sorts it from a to z
+    function sortingLastNames(a, b){
+        if(a.last < b.last){
+            return -1;
+        }
+        if(a.last > b.last){
+            return 1;
+        }
+        return 0;
+    }
+
+    //function does not work correctly as the json data is a string
+    function sortBalance(a, b){
+        if(a.balance < b.balance){
+            return -1;
+        }
+        if(a.balance > b.balance){
+            return 1;
+        }
+        return 0;
+    }
+
+    //function compares the dates and sorts them from awhile ago to recently
+    function compareDates(a, b){
+        return new Date(a.created).getTime() - new Date(b.created).getTime();
+    }
 }
 
 //calling function
