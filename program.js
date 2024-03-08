98aa1bb6faf94ccef123b6a4cd9b8c2be3e8e30e
chrisalv2437 <162181147+chrisalv2437@users.noreply.github.com>
2024-03-07 18:48:49 -0800

    In html: moving the data towards the bottom in Javascript: adding functions to sort data

--- program.js
+++ program.js
@@ -1,26 +1,15 @@
-/////////////////////////////////////
-//Name: Christian Alvarado
-//Filename: program.js
-//Editor: Visual Studio Code
-//Language: Javascript
-//Extension: Live server
-//OS: Windows 10
-//Purpose: reaching out to randomapi.com, fetching the data in JSON format,
-//print out a user friendly interface that will sort data for user
-//Operational status: not working
-/////////////////////////////////////
-
-//Defining API URL to call
-
 //Fetch function to make a GET request to API URL returning a promise
 async function getResults(){
+
     const dataDisplay = document.getElementById('dataDisplay');
+
     fetch("https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb?fmt=json", {
         method: 'GET',
         headers:{
             'Accept' : "application/json",
         },
     })
+
     //then method handles the asynchoronous response from server
     .then(response => {
         //checks to ensure response is valid
@@ -36,13 +25,64 @@ async function getResults(){
         //parse the JSON data using method below
         return response.json()
     })
+
     //displaying the data onto the webpage
     .then(data => {
+
+        //defining results to sort
+        let myResults = data.results;
+        let element = myResults[0];
+
+        //displaying information
         const dataDisplay = document.getElementById('dataDisplay');
         const informationElement = document.createElement("p");
-        informationElement.textContent = JSON.stringify(data.results);
+        informationElement.textContent = "" + JSON.stringify(element);
         dataDisplay.appendChild(informationElement);
-        console.log(Object.values(data.results));
+
+        //function compares the first names and sorts it from a to z
+        function sortingFirstNames(a, b){
+            if(a.first < b.first){
+                return -1;
+            }
+            if(a.first > b.first){
+                return 1;
+            }
+            return 0;
+        }
+
+        //function compares the last names and sorts it from a to z
+        function sortingLastNames(a, b){
+            if(a.last < b.last){
+                return -1;
+            }
+            if(a.last > b.last){
+                return 1;
+            }
+            return 0;
+        }
+
+        //function does not work correctly as the json data is a string
+        function sortBalance(a, b){
+            if(a.balance < b.balance){
+                return -1;
+            }
+            if(a.balance > b.balance){
+                return 1;
+            }
+            return 0;
+        }
+
+        //function compares the dates and sorts them from awhile ago to recently
+        function compareDates(a, b){
+            return new Date(a.created).getTime() - new Date(b.created).getTime();
+        }
+
+        //using sort function for first names
+        element.sort(sortingFirstNames);
+        
+        //logging it to the console and presenting it in table format
+        console.table(element);
+
     })
     //handle any errors that may occur
     .catch(error => {
@@ -50,30 +90,5 @@ async function getResults(){
     });
 }
 
+//calling function
 getResults();
-
-function compareFirstName(a, b){
-    a = a.toLowerCase();
-    b = b.toLowerCase();
-
-    if(a.first < b.first){
-        return -1;
-    }
-    if(a.first > b.first){
-        return 1;
-    }
-    return 0;
-}
-
-function compareLastName(a, b){
-    a = a.toLowerCase();
-    b = b.toLowerCase();
-
-    if(a.last < b.last){
-        return -1;
-    }
-    if(a.last > b.last){
-        return 1;
-    }
-    return 0;
-}
